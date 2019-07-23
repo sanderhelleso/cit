0d9f6ea5218f98f18e5d7f1eb064da1edf443af1
Sanderhelleso <sanderhelleso@hotmail.com>
2019-07-24 00:21:21 +0200

    Adjusted commands

--- index.js
+++ index.js
@@ -5,30 +5,25 @@ const branchName = require('current-git-branch');
 const program = new commander.Command();
 program.version('1.0.0', '-v', '--version');
 
-program.option('branch, --branch', 'Get the name of the current branch');
+program.command('branch').action(() => console.log(`Current branch: ${getBranch()}`));
 program.command('commit <message>').option('-a, --add', 'Add all files to git before committing').action(commit);
 
 program.parse(process.argv);
-
-/**
- * Logs the current branch of active git repo
- */
 
 function getBranch() {
 	const branch = branchName();
 	return branch ? branch : 'Master';
 }
 
-if (program.branch) {
-	console.log(`Current branch: ${getBranch()}`);
-}
-
-/**
- * Commit and append current branch as prefix to commit message
- */
-
 function commit(message, { add }) {
-	if (add) exe('git add .');
+	if (add) {
+		const added = exe('git add .');
+		if (!added) {
+			return shell.echo('Error: Git add failed');
+		} else {
+			shell.echo('Success: Files added to commit!');
+		}
+	}
 
 	const branch = getBranch();
 	const git_message = `${branch}: ${message}`;
@@ -37,7 +32,7 @@ function commit(message, { add }) {
 	if (!committed) {
 		shell.echo('Error: Git commit failed');
 	} else {
-		shell.echo(`Successfully committed to ${branch}`);
+		shell.echo(`Successfully committed to ${branch}!`);
 	}
 }
 
